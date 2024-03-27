2f0be84cac5020e2664b0c9efc232770fbbca6d7
Tamás <150516944+Marsal99@users.noreply.github.com>
2024-03-27 13:35:46 +0100

    a bejelntkezési fül felugróvá tétele

--- neptun-plus/public/index.php
+++ neptun-plus/public/index.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Neptun+</title>
     <style>
-
-        
         /* CSS a felugró ablakhoz */
         .popup {
             display: none;
