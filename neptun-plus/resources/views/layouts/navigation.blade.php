3c81f1291720c2297ba2fd3b14276a2604015338
barnapet <barnapet00@gmail.com>
2024-04-26 15:39:00 +0200

    Igazolás leadás menüpontot csak diák láthatja

--- neptun-plus/resources/views/layouts/navigation.blade.php
+++ neptun-plus/resources/views/layouts/navigation.blade.php
@@ -20,9 +20,12 @@
 						Kurzusok
 					</x-nav-link>
 
-                    <x-nav-link :href="route('submit_certificate')" :active="request()->routeIs('submit_certificate')">
-                        Igazolás leadás
-                    </x-nav-link>
+                    {{-- Roles: Teacher - 1, Student - 2 --}}
+                    @if (Auth::user()->role_id === 2)
+                        <x-nav-link :href="route('submit_certificate')" :active="request()->routeIs('submit_certificate')">
+                            Igazolás leadás
+                        </x-nav-link>
+                    @endif
 
                     {{-- Roles: Teacher - 1, Student - 2 --}}
                     @if (Auth::user()->role_id === 1)
