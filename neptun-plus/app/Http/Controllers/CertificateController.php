cbe8fe8bb67e5180e5bf33a0f1753a95c89170cc
barnapet <barnapet00@gmail.com>
2024-04-27 06:08:44 +0200

    Az igazolás leadás metódusok módosítása

--- neptun-plus/app/Http/Controllers/CertificateController.php
+++ neptun-plus/app/Http/Controllers/CertificateController.php
@@ -4,22 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Certificate;
+use Illuminate\Support\Facades\Storage;
 
 class CertificateController extends Controller
 {
-    /**
-     * Show the form for submitting a certificate.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function submit()
-    {
-        $user = Auth::user()->load(['role', 'grades', 'courses', 'courses.subject', 'attendances']);
+	public function certificate_view()
+	{
+		$user = Auth::user()->load(['role', 'grades', 'courses', 'attendances']);
 
 		$additionalData = [
 			'user' => $user,
 		];
 
 		return view('submit_certificate')->with($additionalData);
+	}
+
+    public function upload_view()
+	{
+		$user = Auth::user()->load(['role', 'grades', 'courses', 'attendances']);
+
+		$additionalData = [
+			'user' => $user,
+		];
+
+		return view('upload_certificate')->with($additionalData);
+	}
+
+public function uploadCertificate(Request $request)
+{
+    if ($request->hasFile('certificate')) {
+        $request->validate([
+            'certificate' => 'required|mimes:pdf',
+        ]);
+
+        $path = $request->file('certificate')->store('public/certificates');
+
+        Certificate::create([
+            'user_id' => auth()->user()->id,
+            'file_path' => $path,
+        ]);
+
+        return redirect()->back()->with('success', 'Az igazolás sikeresen feltöltve.');
     }
+
+    return redirect()->back()->with('error', 'Nem található feltöltött fájl.');
+}
+
 }
