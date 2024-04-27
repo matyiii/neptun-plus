ca647bfb466a8eda65c6487e82c7d98f3ffde0bd
barnapet <barnapet00@gmail.com>
2024-04-27 23:11:53 +0200

    Upload certificate és web.php javítása

--- neptun-plus/resources/views/upload_certificate.blade.php
+++ neptun-plus/resources/views/upload_certificate.blade.php
@@ -1,3 +1,5 @@
+<!-- resources/views/upload_certificate.blade.php -->
+
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -5,6 +7,16 @@
                 <div class="p-6 text-gray-900">
                     <h1 class="text-2xl mb-4 font-bold">Igazolás feltöltése</h1>
                     <div class="overflow-x-auto">
+                        @if(session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                        @if(session('error'))
+                            <div class="alert alert-danger">
+                                {{ session('error') }}
+                            </div>
+                        @endif
                         <table class="min-w-full divide-y divide-gray-200">
                             <form action="{{ route('certificate.upload.pdf') }}" method="post" enctype="multipart/form-data">
                                 @csrf
